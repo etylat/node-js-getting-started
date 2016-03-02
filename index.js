3ac5693e7b34bdee3bcf1315d70efd7e7d07fe7e
etylat <etylat@yahoo.com.vn>
2016-03-02 23:35:39 +0700

    proxy

--- index.js
+++ index.js
@@ -1,20 +1,69 @@
-var express = require('express');
-var app = express();
+var util = require('util'),
+    colors = require('colors'),
+    http = require('http'),
+    connect = require('connect'),
+    httpProxy = require('http-proxy');
 
-app.set('port', (process.env.PORT || 5000));
+var host = 'localhost:8002/';
+var selects = [];
+var simpleselect = {};
 
-app.use(express.static(__dirname + '/public'));
+simpleselect.query = '*[src]';
+simpleselect.func = function (node) {
+  var srctmp = node.getAttribute('src');
+  srctmp = srctmp.replace('://', '://' + host);
+  node.setAttribute('src', srctmp);
+}
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+selects.push(simpleselect);
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+simpleselect.query = '*[href]';
+simpleselect.func = function (node) {
+  var srctmp = node.getAttribute('href');
+  srctmp = srctmp.replace('://', '://' + host);
+  node.setAttribute('href', srctmp);
+}
+
+selects.push(simpleselect);
+
+var app = connect();
+
+//
+// Http Server with proxyRequest Handler and Latency
+//
+var proxy = new httpProxy.createProxyServer();
+
+app.use(require('harmon')([], selects));
+
+app.use(function (req, res) {
+  delete req.headers.host;
+  var url = req.url;
+  req.url = '';
+  delete res.headers['content-length'];
+  proxy.web(req, res, { target: 'http:/' + url });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+http.createServer(app).listen(8002);
+
+
+
+
+proxy.on('error', function (err, req, res) {
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+  
+  res.end('Something went wrong. And we are reporting a custom error message.');
 });
 
+//
+// Target Http Server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied to: ' + req.url + '\n' + JSON.stringify(req.headers, true, 2));
+  res.end();
+}).listen(9002);
 
+util.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '8002 '.yellow + 'with proxy.web() handler'.cyan.underline + ' and latency'.magenta);
+util.puts('http server '.blue + 'started '.green.bold + 'on port '.blue + '9001 '.yellow);
